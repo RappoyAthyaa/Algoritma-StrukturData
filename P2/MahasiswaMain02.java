f55c7f2e6efe5ed76580623f593610875707b635
RappoyAthyaa <raffbrtd.18@gmail.com>
2026-02-25 17:10:17 +0700

    P2_Tugas3_Pertanyaan_No5

--- P2/MahasiswaMain02.java
+++ P2/MahasiswaMain02.java
@@ -9,12 +9,24 @@ public class MahasiswaMain02 {
         mhs1.ipk = 3.55;
 
         mhs1.tampilkanInformasi();
+        System.out.println(mhs1.nilaiKinerja());
+        System.out.println();
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIpk(3.60);
         mhs1.tampilkanInformasi();
+        System.out.println(mhs1.nilaiKinerja());
+
+        System.out.println();
 
         Mahasiswa02 mhs2 = new Mahasiswa02("Annisa Nabila", "2141720160", 3.25, "TI 2L");
         mhs2.updateIpk(3.30);
         mhs2.tampilkanInformasi();
+        System.out.println(mhs2.nilaiKinerja());
+
+        System.out.println();
+
+        Mahasiswa02 mhs3 = new Mahasiswa02("Ahmad Subhan", "2241720129", 3.75, "SI 2A");
+        mhs3.tampilkanInformasi();
+        System.out.println(mhs3.nilaiKinerja());
     }
 }
