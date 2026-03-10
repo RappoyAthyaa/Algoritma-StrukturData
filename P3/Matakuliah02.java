3e348b5f6bb778153dd15fd90aed187e5bbcc6cb
RappoyAthyaa <raffbrtd.18@gmail.com>
2026-03-10 21:25:08 +0700

    P3_Tugas3_Pertanyaan_No2

--- P3/Matakuliah02.java
+++ P3/Matakuliah02.java
@@ -1,5 +1,7 @@
 package P3;
 
+import java.util.Scanner;
+
 public class Matakuliah02 { 
     public String kode;
     public String nama;
@@ -11,5 +13,18 @@ public class Matakuliah02 {
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
+    }
+
+    public Matakuliah02() {}
+
+    public void tambahData(Scanner scanner) {
+        System.out.print("Kode      : ");
+        this.kode = scanner.nextLine();
+        System.out.print("Nama      : ");
+        this.nama = scanner.nextLine();
+        System.out.print("Sks       : ");
+        this.sks = Integer.parseInt(scanner.nextLine());
+        System.out.print("JumlahJam : ");
+        this.jumlahJam = Integer.parseInt(scanner.nextLine());
     }
 }
