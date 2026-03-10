7ffba9154fa349f60683ccc21aaa0fd3a1226156
RappoyAthyaa <raffbrtd.18@gmail.com>
2026-03-10 21:33:27 +0700

    P3_Tugas3_Pertanyaan_No2 (Main)

--- P3/MatakuliahDemo02.java
+++ P3/MatakuliahDemo02.java
@@ -6,24 +6,12 @@ public class MatakuliahDemo02 {
     public static void main(String[] args) {
         Scanner rappyAthaya = new Scanner(System.in);
         Matakuliah02[] arrayOfMatakuliah = new Matakuliah02[3];
-        String kode, nama, dummy;
-        int sks, jumlahJam;
 
         for (int i = 0; i < 3; i++) {
             System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
-            System.out.print("Kode      : ");
-            kode = rappyAthaya.nextLine();
-            System.out.print("Nama      : ");
-            nama = rappyAthaya.nextLine();
-            System.out.print("Sks       : ");
-            dummy = rappyAthaya.nextLine();
-            sks = Integer.parseInt(dummy);
-            System.out.print("JumlahJam : ");
-            dummy = rappyAthaya.nextLine();
-            jumlahJam = Integer.parseInt(dummy);
+            arrayOfMatakuliah[i] = new Matakuliah02();       
+            arrayOfMatakuliah[i].tambahData(rappyAthaya);   
             System.out.println("-----------------------------");
-
-            arrayOfMatakuliah[i] = new Matakuliah02 (kode, nama, sks, jumlahJam);
         }
 
         for (int i = 0; i < arrayOfMatakuliah.length; i++) {
