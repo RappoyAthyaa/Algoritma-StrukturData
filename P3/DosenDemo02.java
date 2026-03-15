215b62e3272cef368781d73e0f8f15b80d8d22cd
RappoyAthyaa <raffbrtd.18@gmail.com>
2026-03-15 15:48:49 +0700

    P3_Tugas 1 (Main)

---
+++ P3/DosenDemo02.java
@@ -0,0 +1,49 @@
+package P3;
+
+import java.util.Scanner;
+
+public class DosenDemo02 {
+    public static void main(String[] args) {
+        Scanner rappyAthaya = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah dosen: ");
+        int n = rappyAthaya.nextInt();
+        rappyAthaya.nextLine(); 
+
+        Dosen02[] arrayOfDosen = new Dosen02[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Masukkan Data Dosen ke-" + (i + 1));
+
+            System.out.print("Kode          : ");
+            String kode = rappyAthaya.nextLine();
+
+            System.out.print("Nama          : ");
+            String nama = rappyAthaya.nextLine();
+
+            System.out.print("Jenis Kelamin : ");
+            String inputJK = rappyAthaya.nextLine();
+            boolean jenisKelamin = inputJK.equalsIgnoreCase("Pria");
+
+            System.out.print("Usia          : ");
+            int usia = rappyAthaya.nextInt();
+            rappyAthaya.nextLine();
+
+            System.out.println("------------------------------");
+
+            arrayOfDosen[i] = new Dosen02(kode, nama, jenisKelamin, usia);
+        }
+
+        int no = 1;
+        for (Dosen02 d : arrayOfDosen) {
+            String jk = d.jenisKelamin ? "Pria" : "Wanita";
+            System.out.println(" Data Dosen ke-" + no);
+            System.out.println("Kode          : " + d.kode);
+            System.out.println("Nama          : " + d.nama);
+            System.out.println("Jenis Kelamin : " + jk);
+            System.out.println("Usia          : " + d.usia);
+            System.out.println("------------------------------");
+            no++;
+        }
+    }
+}
