93eef1f4098b07cb5cc64d40759269368dd61a3f
RappoyAthyaa <raffbrtd.18@gmail.com>
2026-04-09 19:48:38 +0700

    P6_Tugas2_Pertanyaan_No2 (ClassBerprestasi)

--- P6/MahasiswaBerprestasi02.java
+++ P6/MahasiswaBerprestasi02.java
@@ -1,32 +1,36 @@
 package P6;
 
 public class MahasiswaBerprestasi02 {
-    Mahasiswa02 [] listMhs = new Mahasiswa02[5];
+    Mahasiswa02[] listMhs;
     int idx;
 
-    void tambah (Mahasiswa02 m) {
-        if (idx<listMhs.length) {
-            listMhs[idx]=m;
+    void setUkuran(int n) {
+        listMhs = new Mahasiswa02[n];
+    }
+
+    void tambah(Mahasiswa02 m) {
+        if (idx < listMhs.length) {
+            listMhs[idx] = m;
             idx++;
-        }else {
-            System.out.println("data sudah penuh");
+        } else {
+            System.out.println("Data sudah penuh");
         }
     }
 
-    void tampil (){
+    void tampil() {
         for (Mahasiswa02 m : listMhs) {
             m.tampilkanInformasi();
             System.out.println("----------------------");
         }
     }
 
-    void bubbleSort(){
-        for (int i = 0; i < listMhs.length-1; i++) {
-            for (int j = 1; j < listMhs.length-i; j++) {
-                if (listMhs[j].ipk>listMhs[j-1].ipk) {
+    void bubbleSort() {
+        for (int i = 0; i < listMhs.length - 1; i++) {
+            for (int j = 1; j < listMhs.length - i; j++) {
+                if (listMhs[j].ipk > listMhs[j - 1].ipk) {
                     Mahasiswa02 tmp = listMhs[j];
-                    listMhs[j]=listMhs[j-1];
-                    listMhs[j-1]=tmp;
+                    listMhs[j] = listMhs[j - 1];
+                    listMhs[j - 1] = tmp;
                 }
             }
         }
