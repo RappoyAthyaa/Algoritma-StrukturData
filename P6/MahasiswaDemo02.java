f717524ff70678a3fc9e1bb935c98ac79b3f41d8
RappoyAthyaa <raffbrtd.18@gmail.com>
2026-04-10 10:46:06 +0700

    P7_Tugas_Latihan 1 (Main)

--- P6/MahasiswaDemo02.java
+++ P6/MahasiswaDemo02.java
@@ -4,60 +4,93 @@ import java.util.Scanner;
 
 public class MahasiswaDemo02 {
     public static void main(String[] args) {
-        Scanner rappyAthya = new Scanner(System.in);
         MahasiswaBerprestasi02 list = new MahasiswaBerprestasi02();
+        Scanner rappyAthya = new Scanner(System.in);
+        int jumMhs=5;
 
-        System.out.print("Masukkan jumlah mahasiswa: ");
-        int n = rappyAthya.nextInt();
-        rappyAthya.nextLine(); 
+        list.setUkuran(jumMhs);
 
-        list.setUkuran(n); 
-
-        for (int i = 1; i <= n; i++) {
-            if (i == 1) {
-            System.out.print("Masukkan Data Mahasiswa ke-" + i + "\n");
-            } else {
-            System.out.print("Masukkan Data Mahasiswa ke-" + i + "\n");
-            }
-
-        System.out.print("NIM   : ");
-        String nim = rappyAthya.nextLine();
-
-        System.out.print("Nama  : ");
-        String nama = rappyAthya.nextLine();
-
-        System.out.print("Kelas : ");
-        String kelas = rappyAthya.nextLine();
-
-        System.out.print("IPK   : ");
-        double ipk = Double.parseDouble(rappyAthya.nextLine());
-        System.out.println("--------------------------------");
-
-        Mahasiswa02 m = new Mahasiswa02(nim, nama, kelas, ipk);
-        list.tambah(m);
-    }
-
-        System.out.println("\n=============================");
-        System.out.println("Data Mahasiswa Sebelum Sorting:");
-        System.out.println("=============================");
+        for (int i = 0; i < jumMhs; i++) {
+            System.out.println("Masukkan Data Mahasiswa ke-" + (i + 1));
+            System.out.print("NIM     : ");
+            String nim = rappyAthya.nextLine();
+            System.out.print("Nama    : ");
+            String nama = rappyAthya.nextLine();
+            System.out.print("Kelas   : ");
+            String kelas = rappyAthya.nextLine();
+            System.out.print("IPK     : ");
+            String ip = rappyAthya.nextLine();
+            Double ipk = Double.parseDouble(ip);
+            System.out.println("--------------------------------");
+            list.tambah (new Mahasiswa02(nim, nama, kelas, ipk));
+        }
         list.tampil();
+        System.out.println("-----------------------------------------------------");
+        System.out.println("Pencarian data");
+        System.out.println("-----------------------------------------------------");
+        System.out.println("masukkan ipk mahasiswa yang dicari: ");
+        System.out.print("IPK: ");
+        double cari = rappyAthya.nextDouble();
 
-        list.bubbleSort();
-        System.out.println("=============================");
-        System.out.println("Data Mahasiswa Setelah Sorting (IPK DESC):");
-        System.out.println("=============================");
-        list.tampil();
-
-        list.selectionSort();
-        System.out.println("=============================");
-        System.out.println("Data yang sudah terurut menggunakan SELECTION SORT (ASC)");
-        System.out.println("=============================");
-        list.tampil();
-
-        list.insertionSort();
-        System.out.println("=============================");
-        System.out.println("Data yang sudah terurut menggunakan INSERTION SORT (ASC)");
-        System.out.println("=============================");
-        list.tampil();
+        System.out.println("menggunakan sequential searching");
+        double posisi = list.sequentialSearching(cari);
+        int pss= (int)posisi;
+        list.tampilPosisi(cari, pss);
+        list.tampilDataSearch(cari, pss);
     }
 }
+
+        // System.out.print("Masukkan jumlah mahasiswa: ");
+        // int n = rappyAthya.nextInt();
+        // rappyAthya.nextLine(); 
+
+        // list.setUkuran(n); 
+
+        
+
+    //     for (int i = 1; i <= n; i++) {
+    //         if (i == 1) {
+    //         System.out.print("Masukkan Data Mahasiswa ke-" + i + "\n");
+    //         } else {
+    //         System.out.print("Masukkan Data Mahasiswa ke-" + i + "\n");
+    //         }
+
+    //     System.out.print("NIM   : ");
+    //     String nim = rappyAthya.nextLine();
+
+    //     System.out.print("Nama  : ");
+    //     String nama = rappyAthya.nextLine();
+
+    //     System.out.print("Kelas : ");
+    //     String kelas = rappyAthya.nextLine();
+
+    //     System.out.print("IPK   : ");
+    //     double ipk = Double.parseDouble(rappyAthya.nextLine());
+    //     System.out.println("--------------------------------");
+
+    //     Mahasiswa02 m = new Mahasiswa02(nim, nama, kelas, ipk);
+    //     list.tambah(m);
+    // }
+
+    //     System.out.println("\n=============================");
+    //     System.out.println("Data Mahasiswa Sebelum Sorting:");
+    //     System.out.println("=============================");
+    //     list.tampil();
+
+    //     list.bubbleSort();
+    //     System.out.println("=============================");
+    //     System.out.println("Data Mahasiswa Setelah Sorting (IPK DESC):");
+    //     System.out.println("=============================");
+    //     list.tampil();
+
+    //     list.selectionSort();
+    //     System.out.println("=============================");
+    //     System.out.println("Data yang sudah terurut menggunakan SELECTION SORT (ASC)");
+    //     System.out.println("=============================");
+    //     list.tampil();
+
+    //     list.insertionSort();
+    //     System.out.println("=============================");
+    //     System.out.println("Data yang sudah terurut menggunakan INSERTION SORT (ASC)");
+    //     System.out.println("=============================");
+    //     list.tampil();
