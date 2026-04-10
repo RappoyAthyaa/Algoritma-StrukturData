9f06c0ba31ef47e245c1659026cfb59359b4a4b8
RappoyAthyaa <raffbrtd.18@gmail.com>
2026-04-10 21:49:59 +0700

    P7_Tugas2_Pertanyaan_No5 (ClassBerprestasi)

--- P6/MahasiswaBerprestasi02.java
+++ P6/MahasiswaBerprestasi02.java
@@ -101,7 +101,7 @@ public class MahasiswaBerprestasi02 {
             if (cari ==listMhs[mid].ipk) {
                 return (mid);
             }
-            else if (listMhs[mid].ipk>cari) {
+            else if (listMhs[mid].ipk < cari) {
                 return findBinarySearch(cari, left, mid-1);
             }
             else {
