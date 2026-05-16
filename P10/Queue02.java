c0083e38688b105fa9c5137e60541e7681768685
RappoyAthyaa <raffbrtd.18@gmail.com>
2026-05-16 21:27:39 +0700

    P10_Tugas1_Pertanyaan_No 7

--- P10/Queue02.java
+++ P10/Queue02.java
@@ -7,27 +7,19 @@ public class Queue02 {
     int size;
     int max;
 
-    public Queue02 (int n) {
+    public Queue02(int n) {
         max = n;
         data = new int[max];
         size = 0;
-        front = rear = -1; 
+        front = rear = -1;
     }
 
     public boolean IsEmpty() {
-        if (size == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return size == 0;
     }
 
-    public boolean IsFull () {
-        if (size == max) {
-            return true;
-        } else {
-            return false;
-        }
+    public boolean IsFull() {
+        return size == max;
     }
 
     public void peek() {
@@ -44,7 +36,7 @@ public class Queue02 {
         } else {
             int i = front;
             while (i != rear) {
-                System.out.println(data[i] + " ");
+                System.out.print(data[i] + " ");
                 i = (i + 1) % max;
             }
             System.out.println(data[i] + " ");
@@ -64,38 +56,26 @@ public class Queue02 {
 
     public void Enqueue(int dt) {
         if (IsFull()) {
-            System.out.println("Queue sudah penuh");
-        } else {
-            if (IsEmpty()) {
-                front = rear = 0;
-            }else {
-                if (rear == max - 1) {
-                    rear = 0;
-                } else {
-                    rear++;
-                }
-            } 
-            data[rear] = dt;
-            size++;
+            System.out.println("OVERFLOW: Queue sudah penuh! Program dihentikan.");
+            System.exit(1);
         }
+        if (front == -1) front = 0;
+        rear = (rear + 1) % max;
+        data[rear] = dt;
+        size++;
     }
 
     public int Dequeue() {
-        int dt = 0;
         if (IsEmpty()) {
-            System.out.println("Queue masih kosong");
+            System.out.println("UNDERFLOW: Queue masih kosong! Program dihentikan.");
+            System.exit(1);
+        }
+        int dt = data[front];
+        size--;
+        if (IsEmpty()) {
+            front = rear = -1;
         } else {
-            dt = data[front];
-            size--;
-            if (IsEmpty()) {
-                front = rear = -1;
-            } else {
-                if (front == max -1) {
-                    front = 0;
-                } else {
-                    front++;
-                }
-            }
+            front = (front + 1) % max;
         }
         return dt;
     }
