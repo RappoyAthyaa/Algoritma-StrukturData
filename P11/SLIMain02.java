1ba76fbaca569f4c2000418df1ae84ea57515838
RappoyAthyaa <raffbrtd.18@gmail.com>
2026-05-22 09:39:50 +0700

    P11_Tugas2_Latihan 2 (Main)

--- P11/SLIMain02.java
+++ P11/SLIMain02.java
@@ -1,65 +1,28 @@
 package P11;
 
-import java.util.Scanner;
-
 public class SLIMain02 {
     public static void main(String[] args) {
-        Scanner rappyAthya = new Scanner(System.in);
         SingleLinkedList02 sll = new SingleLinkedList02();
-        int pilihan;
 
-        do {
-            System.out.println("\n===== MENU LINKED LIST =====");
-            System.out.println("1. Add First");
-            System.out.println("2. Add Last");
-            System.out.println("3. Insert After");
-            System.out.println("4. Insert At (index)");
-            System.out.println("5. Tampilkan List");
-            System.out.println("0. Keluar");
-            System.out.print("Pilihan: ");
-            pilihan = rappyAthya.nextInt();
-            rappyAthya.nextLine(); 
+        Mahasiswa02 mhs0 = new Mahasiswa02("21212200", "Andi", "3C", 3.2);  
+        Mahasiswa02 mhs1 = new Mahasiswa02("22212202", "Cintia", "3C", 3.5); 
+        Mahasiswa02 mhs2 = new Mahasiswa02("23212201", "Bimon", "2B", 3.8);  
 
-            if (pilihan >= 1 && pilihan <= 4) {
-                System.out.print("Nama   : "); String nama = rappyAthya.nextLine();
-                System.out.print("NIM    : "); String nim = rappyAthya.nextLine();
-                System.out.print("Kelas  : "); String kelas = rappyAthya.nextLine();
-                System.out.print("IPK    : "); double ipk = rappyAthya.nextDouble();
-                rappyAthya.nextLine();
-                Mahasiswa02 mhs = new Mahasiswa02(nim, nama, kelas, ipk);
+        sll.addFirst(mhs0);
+        sll.addLast(mhs1);
+        sll.addLast(mhs2);
 
-                switch (pilihan) {
-                    case 1:
-                        sll.addFirst(mhs);
-                        System.out.println("Data berhasil ditambahkan di awal.");
-                        break;
-                    case 2:
-                        sll.addLast(mhs);
-                        System.out.println("Data berhasil ditambahkan di akhir.");
-                        break;
-                    case 3:
-                        System.out.print("Masukkan nama setelah node mana: ");
-                        String key = rappyAthya.nextLine();
-                        sll.insertAfter(key, mhs);
-                        System.out.println("Data berhasil disisipkan setelah " + key);
-                        break;
-                    case 4:
-                        System.out.print("Masukkan index: ");
-                        int index = rappyAthya.nextInt();
-                        rappyAthya.nextLine();
-                        sll.insertAt(index, mhs);
-                        System.out.println("Data berhasil disisipkan di index " + index);
-                        break;
-                }
-            } else if (pilihan == 5) {
-                sll.print();
-            } else if (pilihan != 0) {
-                System.out.println("Pilihan tidak valid!");
-            }
+        System.out.println("data index 1 : ");
+        sll.getData(1);
+        System.out.println();
 
-        } while (pilihan != 0);
+        System.out.println("data mahasiswa an Bimon berada pada index : " + sll.indexDf("bimon"));
+        System.out.println();
 
-        System.out.println("Program selesai.");
-        rappyAthya.close();
+        sll.removeFirst();
+        sll.print();
+
+        sll.removeFirst();
+        sll.print();
     }
 }
