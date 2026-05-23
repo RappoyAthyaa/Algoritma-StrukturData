68fcae7db6463ce96bb653106eab4e2542613d76
RappoyAthyaa <raffbrtd.18@gmail.com>
2026-05-23 10:50:56 +0700

    P11_Tugas 1 (ClassNode)

---
+++ P11/NodeTugas02.java
@@ -0,0 +1,11 @@
+package P11;
+
+public class NodeTugas02 {
+    MahasiswaClass02 data;
+    NodeTugas02 next;
+
+    public NodeTugas02(MahasiswaClass02 data) {
+        this.data = data;
+        this.next = null;
+    }
+}
