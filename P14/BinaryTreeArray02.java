b781772ef7cd6ccea951165872456739205a1d90
RappoyAthyaa <raffbrtd.18@gmail.com>
2026-06-17 00:39:46 +0700

    P14_Tugas_4 (ClassBinaryTreeArray)

--- P14/BinaryTreeArray02.java
+++ P14/BinaryTreeArray02.java
@@ -6,6 +6,7 @@ public class BinaryTreeArray02 {
 
     public BinaryTreeArray02() {
         this.dataMahasiswa = new Mahasiswa02[10];
+        this.idxLast = -1;
     }
 
     void populateData(Mahasiswa02 dataMhs[], int idxLast) {
@@ -16,9 +17,30 @@ public class BinaryTreeArray02 {
     void traverseInOrder(int idxStart) {
         if (idxStart <= idxLast) {
             if (dataMahasiswa[idxStart] != null) {
-                traverseInOrder(2 * idxStart + 1); 
-                dataMahasiswa[idxStart].tampilInformasi(); 
+                traverseInOrder(2 * idxStart + 1);
+                dataMahasiswa[idxStart].tampilInformasi();
                 traverseInOrder(2 * idxStart + 2);
+            }
+        }
+    }
+
+    // Method add() Tugas 4
+    public void add(Mahasiswa02 data) {
+        if (idxLast >= dataMahasiswa.length - 1) {
+            System.out.println("Tree Array sudah penuh!");
+            return;
+        }
+        idxLast++; 
+        dataMahasiswa[idxLast] = data;
+        System.out.println("Berhasil menambahkan " + data.nama + " di indeks ke-" + idxLast);
+    }
+
+    public void traversePreOrder(int idxStart) {
+        if (idxStart <= idxLast) {
+            if (dataMahasiswa[idxStart] != null) {
+                dataMahasiswa[idxStart].tampilInformasi();
+                traversePreOrder(2 * idxStart + 1);
+                traversePreOrder(2 * idxStart + 2);
             }
         }
     }
