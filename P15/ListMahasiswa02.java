d320ae19cc2d94979a68478e53e78e035119b072
RappoyAthyaa <raffbrtd.18@gmail.com>
2026-06-23 23:00:48 +0700

    P15_Tugas3_Pertanyaan_No 2 & 3

--- P15/ListMahasiswa02.java
+++ P15/ListMahasiswa02.java
@@ -24,12 +24,26 @@ public class ListMahasiswa02 {
     }
 
     int linearSearch(String nim) {
-        for (int i = 0; i < mahasiswas.size(); i++) {
-            if (nim.equals(mahasiswas.get(i).nim)) {
+        for (int i = 0; i < mahasiswas.size(); i++) { 
+            if (nim.equals(mahasiswas.get(i).nim)) { 
                 return i;
             }
         }
-        return -1;
+        return -1; 
+    }
+
+    public void sortingAsc() {
+        mahasiswas.sort((m1, m2) -> m1.nim.compareTo(m2.nim));
+    }
+
+    public void sortingDesc() {
+        mahasiswas.sort((m1, m2) -> m2.nim.compareTo(m1.nim));
+    }
+
+    int pakaiBinarySearch(String nim) {
+        this.sortingAsc();
+        Mahasiswa02 cari = new Mahasiswa02(nim, "", "");
+        return java.util.Collections.binarySearch(mahasiswas, cari, (m1, m2) -> m1.nim.compareTo(m2.nim));
     }
 
     public static void main(String[] args) {
@@ -39,10 +53,19 @@ public class ListMahasiswa02 {
         Mahasiswa02 m2 = new Mahasiswa02("201236", "Shannum", "021xx3");
 
         lm.tambah(m, m1, m2);
+        System.out.println("--- Data Awal ---");
         lm.tampil();
-        
+
         lm.update(lm.linearSearch("201235"), new Mahasiswa02("201235", "Akhleema Lela", "021xx2"));
-        System.out.println("");
+        System.out.println("\n--- Setelah Update ---");
         lm.tampil();
+
+        System.out.println("\n--- Uji Coba Pencarian Binary Search ---");
+        int hasilCari = lm.pakaiBinarySearch("201236");
+        if (hasilCari >= 0) {
+            System.out.println("Mahasiswa ditemukan pada indeks ke-" + hasilCari);
+        } else {
+            System.out.println("Mahasiswa tidak ditemukan.");
+        }
     }
 }
